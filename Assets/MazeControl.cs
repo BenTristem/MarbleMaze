a34c06ffa0400c78ea4799871fa01f0dd6391419
Ben Tristem <github@bentristem.com>
2015-08-06 18:45:10 +0100

    Skip mobile refinement for now.

--- Assets/MazeControl.cs
+++ Assets/MazeControl.cs
@@ -7,15 +7,21 @@ public class MazeControl : MonoBehaviour {
 	[Range (0.05f, 1f)]
 	public float sensitivity;
 
+	public bool smoothing;
+	public float smoothTime = 1.0f;
+
+	private Vector3 initialRotation;
+
 	// Use this for initialization
 	void Start () {
-	
+		initialRotation = transform.rotation.eulerAngles;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		float xInput = CrossPlatformInputManager.GetAxis (("Horizontal")) * 90f * sensitivity;
-		float zInput = CrossPlatformInputManager.GetAxis ("Vertical") * 90f * sensitivity;
+	void LateUpdate () {
+		float zInput = -CrossPlatformInputManager.GetAxis (("Horizontal")) * 90f * sensitivity;
+		float xInput = CrossPlatformInputManager.GetAxis ("Vertical") * 90f * sensitivity;
+
 		transform.localRotation = Quaternion.Euler (new Vector3 (xInput, 0f, zInput));
 	}
 }
