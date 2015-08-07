f5ef3ad9ffd2d7f7281973c33836509837a12186
Ben Tristem <github@bentristem.com>
2015-08-07 17:00:36 +0100

    Writing Control Code

---
+++ Assets/TiltControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class TiltControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (CrossPlatformInputManager.GetAxis ("Vertical")) ;
+	}
+}
