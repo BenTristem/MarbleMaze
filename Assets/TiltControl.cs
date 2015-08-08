0d24193d5a4c7fdee1dd8d67bca130d99756d048
Ben Tristem <github@bentristem.com>
2015-08-08 12:52:21 +0100

    Create A Sensitivity Control

--- Assets/TiltControl.cs
+++ Assets/TiltControl.cs
@@ -4,6 +4,8 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class TiltControl : MonoBehaviour {
 
+	public float sensitivity = 90f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -19,6 +21,6 @@ public class TiltControl : MonoBehaviour {
 
 		print (inputValue.x + " , " + inputValue.z);
 
-		transform.eulerAngles = inputValue;
+		transform.eulerAngles = inputValue * sensitivity;
 	}
 }
